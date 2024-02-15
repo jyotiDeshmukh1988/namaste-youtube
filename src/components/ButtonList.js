8226fab56b4f120e40ba9146edd3670d1863d524
Jyoti Deshmukh <test8600896492@gmail.com>
2024-02-15 15:20:04 +0530

    refractor the button and buttonlist component

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Button from './Button'
+
+const buttonitems = ["All","Live","Music","Bollywood Music","Stock Market","BK Shivani","Satsang","Animated Movies"];
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
+    <div className='flex gap-3'>
+      {
+        buttonitems.map((buttonname)=>{
+          return <Button name={buttonname} key={buttonname}/>
+        })
+      }
+    </div>
   )
 }
 
